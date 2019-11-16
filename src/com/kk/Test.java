93d9f994f444873a5c186f65f4a6a8653cf3c4c2
Khagendra <Khagendra@LAPTOP-9EBNEKLL>
2019-11-16 11:13:44 +0530

    this is my first commit in local repository

---
+++ src/com/kk/Test.java
@@ -0,0 +1,11 @@
+package com.kk;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this message is written by GIT master or TL");
+
+	}
+
+}
